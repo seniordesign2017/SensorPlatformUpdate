feb5f9d1de74104286b486c5db298299f734af12
seniordesign2017 <e.spagnoletti@vikes.csuohio.edu>
2017-05-05 11:12:50 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -121,10 +121,10 @@ $(function() {
 								data: data,
 								units: units
 							});
-							
+								changeCurrentValue(last_val[1],friendly);
 						}
 					}
-					changeCurrentValue(last_val[1],friendly);
+				
 				}
 				$("#placeholder").text('');
 				$.plot("#placeholder", data_to_plot, graph_options);
